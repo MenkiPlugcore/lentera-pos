5ff8c7e4ba212c3d48e8c3c29eca46ac0e219fb2
MenkiPlugcore <cfirmansyah327@gmail.com>
2026-08-25 16:55:32 +0700

    feat: mount reports and history panel

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,13 @@ import './styles.css'
 import App from './App.jsx'
 import HardDeletePanel from './HardDeletePanel.jsx'
 import POSPanel from './POSPanel.jsx'
+import ReportsPanel from './ReportsPanel.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
     <HardDeletePanel />
     <POSPanel />
+    <ReportsPanel />
   </StrictMode>,
 )
