2f390aea8a6c2851a75bd5aecce318e46385a507
MenkiPlugcore <cfirmansyah327@gmail.com>
2026-08-25 17:13:41 +0700

    feat: load barcode detector fallback

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles.css'
+import './barcodeDetectorPolyfill.js'
 import App from './App.jsx'
 import HardDeletePanel from './HardDeletePanel.jsx'
 import POSPanel from './POSPanel.jsx'
